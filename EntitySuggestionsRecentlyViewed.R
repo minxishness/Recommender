05bc8d6633d55ace1b8caa39a93495488dae986d
minxishness <draganakuljanin@gmail.com>
2015-04-17 14:35:34 +0100

    Recently Viewed

---
+++ EntitySuggestionsRecentlyViewed.R
@@ -0,0 +1,43 @@
+library(plyr)
+
+#calculate score for an entity pair
+entityMatchScore<-function(entityId,contData, baseEntityId) {    
+    occurances<-sum(contData$entityId == entityId)
+    matchs<-occurances/(nrow(contData))^2
+    baseEntityPos<-which(gsoData$entityId==baseEntityId)[1]
+    suggestionPos<-which(gsoData$entityId==entityId)[1]
+    baseEntityName<-as.character(gsoData$entityName[baseEntityPos])
+    sugEntityName<-as.character(gsoData[suggestionPos,7])
+    return(c(baseEntityId, entityId, matchs,baseEntityName,sugEntityName))
+}
+
+#calculatee scores for all recommendations for an entity
+calcEntitySug<-function(entityId) {
+    #find all groups which contain the entity
+    contGroups<-gsData[gsData$entityId %in% entityId, 2]
+    #find all entites we need to calculate score for
+    contData<-gsData[gsData$groupId %in% contGroups,]
+    entitiesCheck<-contData
+    entitiesCheck<-entitiesCheck[entitiesCheck$entityId!=entityId,1]
+    entitiesCheck<-entitiesCheck[(!duplicated(entitiesCheck))]
+    entityRes<-sapply(entitiesCheck,entityMatchScore, contData=contData, baseEntityId=entityId)
+    return(entityRes)
+}
+
+begTime <- Sys.time()
+gsoData<-read.csv("recentlyViewed17-Apr-2014.csv")
+colnames(gsoData)<-c("userId","entityId","addedDate", "entityName" )
+gsoData$groupId<-paste(gsoData$userId,gsoData$addedDate,sep="")
+#get relevant columns only
+gsData<-gsoData[,c(2,5)]
+#get Unique entity ID's to get suggestions for
+entitiesUnique<-gsData[,1]
+entitiesUnique<-entitiesUnique[(!duplicated(entitiesUnique))]
+comboRes<-sapply(entitiesUnique,calcEntitySug)
+comboRes <- data.frame(matrix(unlist(comboRes), ncol=5, byrow=T))
+colnames(comboRes)<-c("entity1Id", "suggestionId", "score", "EntityName","SuggestionName" )
+comboRes<-arrange(comboRes, desc(score))
+View(comboRes)
+write.csv(comboRes,"newres.csv")
+runTime <- Sys.time()-begTime
+runTime
