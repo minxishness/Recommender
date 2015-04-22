f24b60614e1c844ed6ed87de3108f876d51f01e1
minxishness <draganakuljanin@gmail.com>
2015-04-22 16:49:17 +0100

    Added both calculations to one file - not tested

--- EntitySuggestionsCombined.R
+++ EntitySuggestionsCombined.R
@@ -31,9 +31,9 @@ calcEntitySug<-function(entityId) {
 }
 
 #combine scores 
-combineScores<-function(comboRow, comboResGroups) {
+combineScores<-function(comboRow, groupsRes) {
     score<-comboRow[3][1]
-    groupScore<-comboResGroups[comboResGroups$entity1Id==comboRow[1] & comboResGroups$suggestionId==comboRow[2],3][1]
+    groupScore<-groupsRes[groupsRes$entity1Id==comboRow[1] & groupsRes$suggestionId==comboRow[2],3][1]
     if (!is.na(groupScore))
         score<-as.numeric(score)+as.numeric(groupScore)
     return(c(comboRow[1],comboRow[2], score,comboRow[4],comboRow[5]))
@@ -41,27 +41,57 @@ combineScores<-function(comboRow, comboResGroups) {
     
 
 begTime <- Sys.time()
-gsoData<-read.csv("recentlyViewed17-Apr-2014.csv")
-colnames(gsoData)<-c("userId","entityId","addedDate", "entityName" )
+########################################################################
+#get scores based on recently viewed
+########################################################################
+rwoData<-read.csv("recentlyViewed17-Apr-2014.csv")
+colnames(rwoData)<-c("userId","entityId","addedDate", "entityName" )
 agData<-read.csv("artistGenre.csv")
 agData<-agData[,c(3,1)]
 colnames(agData)<-c("entityId", "genre")
 popPos<-which (agData$genre=="POPXXX")
 agData<-agData[-popPos,]
-gsoData$groupId<-paste(gsoData$userId,gsoData$addedDate,sep="")
+rwoData$groupId<-paste(gsoData$userId,gsoData$addedDate,sep="")
 #get relevant columns only
-gsData<-gsoData[,c(2,5)]
+rwData<-rwoData[,c(2,5)]
 #get Unique entity ID's to get suggestions for
-entitiesUnique<-gsData[,1]
+entitiesUnique<-rwData[,1]
 entitiesUnique<-entitiesUnique[(!duplicated(entitiesUnique))]
-comboRes<-sapply(entitiesUnique,calcEntitySug)
+similarRes<-sapply(entitiesUnique,calcEntitySug)
+similarRes <- data.frame(matrix(unlist(similarRes), ncol=5, byrow=T))
+colnames(similarRes)<-c("entity1Id", "suggestionId", "score", "entityName","suggestionName" )
+similarRes$score<-as.numeric(as.character(similarRes$score))
+
+########################################################################
+#Get scores based on grops
+########################################################################
+gsoData<-read.csv("GroupMembers17Apr2015.csv")
+colnames(gsoData)<-c("favouriteId","userId","entityId","addedDate", "groupId", "isVisible", "entityName", "groupName" )
+gsoData$addedDate<-as.Date(gsoData$addedDate, "%d/%m/%Y")
+gsoData<-arrange(gsoData,desc(addedDate))
+#get relevant columns only
+gsData<-gsoData[,c(3,5)]
+#get Unique entity ID's to get suggestions for
+entUniqueGroups<-gsData[,1]
+entUniqueGroups<-entUniqueGroups[(!duplicated(entUniqueGroups))]
+groupRes<-sapply(entUniqueGroups,calcEntitySug)
+groupRes <- data.frame(matrix(unlist(groupRes), ncol=5, byrow=T))
+colnames(groupRes)<-c("entity1Id", "suggestionId", "score", "EntityName","SuggestionName" )
+groupRes<-sapply(entitiesUnique,calcEntitySug)
+groupRes <- data.frame(matrix(unlist(groupRes), ncol=5, byrow=T))
+colnames(groupRes)<-c("entity1Id", "suggestionId", "score", "EntityName","SuggestionName" )
+groupsRes$score<-as.numeric(as.character(groupsRes$score))
+groupRes<-arrange(groupRes, desc(score))
+
+########################################################################
+#Combine scores
+########################################################################
+comboRes<-apply(comboRes,1, combineScores, groupsRes=groupsRes)
 comboRes <- data.frame(matrix(unlist(comboRes), ncol=5, byrow=T))
-colnames(comboRes)<-c("entity1Id", "suggestionId", "score", "entityName","suggestionName" )
+colnames(comboRes)<-c("entity1Id", "suggestionId", "score", "EntityName","SuggestionName" )
 comboRes$score<-as.numeric(as.character(comboRes$score))
-comboResGroups$score<-as.numeric(as.character(comboResGroups$score))
-comboRes1<-apply(comboRes,1, combineScores, comboResGroups=comboResGroups)
-comboRes<-arrange(comboRes, desc(score))
+groupRes<-arrange(groupRes, desc(score))
 View(comboRes)
-write.csv(comboRes,"newres.csv")
+write.csv(comboRes,"combinedRes.csv")
 runTime <- Sys.time()-begTime
 runTime
